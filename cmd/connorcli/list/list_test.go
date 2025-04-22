26b2f536d1fd248cc50370a8e20377c661586742
Connor Robertson <robertsonc4@gmail.com>
2025-04-22 00:43:52 -0700

    Add test to list with arguments

--- cmd/connorcli/list/list_test.go
+++ cmd/connorcli/list/list_test.go
@@ -24,6 +24,25 @@ func TestListCmd_Execute(t *testing.T) {
 	// Since last modified is not a static value no value validation
 }
 
+func TestListArgsCmd_Execute(t *testing.T) {
+	// Create Command
+	cmd := list.ListCommand()
+
+	// Redirect stdout
+	var stdout bytes.Buffer
+	cmd.SetOut(&stdout)
+	cmd.SetArgs([]string{"./test/"})
+
+	// Catch Errors
+	err := cmd.Execute()
+
+	if err != nil {
+		t.Errorf("Unexpected Error: %v", err)
+	}
+
+	// Since last modified is not a static value no value validation
+}
+
 func TestListAllCmd_Execute(t *testing.T) {
 	// Create Command
 	cmd := list.ListAllCommand()
